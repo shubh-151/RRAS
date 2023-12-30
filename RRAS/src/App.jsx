31a47c1ebf15529fc2b2b3c7d34e25e33383bf92
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-12-30 17:25:17 +0530

    All

--- RRAS/src/App.jsx
+++ RRAS/src/App.jsx
@@ -17,12 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
