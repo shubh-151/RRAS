df0d4e66582320fd3d40343646f383366b0ec74c
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-12-31 16:43:03 +0530

    header updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-
   return (
-    <>
-      <div>
-        
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is shubham shukla
-        </button>
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <>
+  <Header/>
+  </>
+  );
 }
 
-export default App
+export default App;
