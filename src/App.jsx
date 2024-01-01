e1daf873d9f0abd300ce18296a1b18e48e81605e
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2024-01-01 15:08:39 +0530

    All changes related to folder and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import "./App.css";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Home from "./components/Home";
 
 function App() {
   return (
   <>
-  <Header/>
-  {/* <Home/> */}
-  <Contact/>
-
-  {/* <About/> */}
-  <Footer/>
+ 
   </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import "./App.css";
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About';
+import Contact from './components/Contact/Contact'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>} />
+      <Route path='about' element={<About/>} />   
+      <Route path='contact' element={<Contact/>} />  
+    </Route>
+  )
+)
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
